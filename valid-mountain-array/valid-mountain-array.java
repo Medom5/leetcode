f8073e18297c2cdab7d04771c89aefa9294044d6
Moussa Matar <145607268+Medom5@users.noreply.github.com>
2024-07-18 11:31:02 +0300

    Time: 2 ms, Memory: 45.1 MB - LeetHub

---
+++ valid-mountain-array/valid-mountain-array.java
@@ -0,0 +1,38 @@
+class Solution {
+   public static boolean validMountainArray(int[] arr) {
+
+        if (arr.length <= 2) {
+            return false;
+        }
+
+        boolean mountain = false;
+        int counter = 0;
+        for (int i = 0; i < arr.length - 1; i++) {
+            if (arr[i] > arr[i + 1]) {
+                counter++;
+            }
+            if (arr[i]==arr[i+1]) {
+                return false;
+            }
+        }
+        if ( counter == arr.length-1) {
+            return false;
+        }
+        for (int i = 1; i < arr.length; i++) {
+
+
+            if (arr[i] < arr[i - 1]) {
+
+                mountain = true;
+            }
+
+            if (mountain && arr[i] > arr[i-1]) {
+                return false;
+            }
+
+        }
+        return mountain;
+
+    }
+
+}
