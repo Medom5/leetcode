cf752ac9f43698bfebb568f3e015d9b1aa45e22c
Moussa Matar <145607268+Medom5@users.noreply.github.com>
2024-08-23 09:57:43 +0300

    Time: 2 ms, Memory: 41.2 MB - LeetHub

---
+++ pascals-triangle-ii/pascals-triangle-ii.java
@@ -0,0 +1,20 @@
+class Solution {
+    public List<Integer> getRow(int rowIndex) {
+     
+        int numRows=rowIndex+1;
+        List<List<Integer>> pascal = new ArrayList<>();
+        for (int i = 1; i <= numRows; i++) {
+            List<Integer> sublist = new ArrayList<>(Collections.nCopies(i, 1));
+            pascal.add(sublist);
+        }
+
+        for (int j = 2; j < numRows; j++) {
+            List<Integer> previous = pascal.get(j - 1);
+            List<Integer> list = pascal.get(j);
+            for (int i = 1; i < list.size() - 1; i++) {
+                list.set(i, previous.get(i - 1) + previous.get(i));
+            }
+        }
+        return pascal.get(rowIndex);
+    }
+}
