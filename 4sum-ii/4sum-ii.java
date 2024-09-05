41f76791d0ca383e46b913202e3055954bc52d2f
Moussa Matar <145607268+Medom5@users.noreply.github.com>
2024-09-05 13:17:01 +0300

    Time: 165 ms, Memory: 44.8 MB - LeetHub

---
+++ 4sum-ii/4sum-ii.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int fourSumCount(int[] nums1, int[] nums2, int[] nums3, int[] nums4) {
+        Map<Long, Integer> map = new HashMap<>();
+        int count=0;
+        
+        for(int i=0; i < nums1.length; i++){
+            for(int j=0; j < nums2.length; j++){
+                long key = nums1[i] + nums2[j];
+                map.put(key,map.getOrDefault(key,0)+1);     
+            }
+        }
+        
+        for(int i=0; i < nums3.length; i++){
+            for(int j=0; j < nums4.length; j++){
+                long key = nums3[i] + nums4[j];
+                if(map.getOrDefault(-key, 0) != 0){
+                    count+=map.get(-key);
+                }
+            }
+        }
+    return count;
+    }    
+}
