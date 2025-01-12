511187926ccf706bf484fae89fb27a7aaaac575a
Moussa Matar <145607268+Medom5@users.noreply.github.com>
2025-01-12 23:00:09 +0200

    Time: 0 ms, Memory: 40.3 MB - LeetHub

---
+++ 000k-th-symbol-in-grammar/000k-th-symbol-in-grammar.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int kthGrammar(int n, int k) {
+        if(n == 1)
+            return 0;
+        
+        int parent = kthGrammar(n-1, k/2 + k%2);
+        
+        if(parent == 1)
+            return k % 2 == 1 ? 1 : 0;
+        else
+            return k % 2 == 1 ? 0 : 1;
+    }
+}
